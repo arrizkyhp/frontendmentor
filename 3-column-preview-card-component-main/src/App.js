d2ca5e04bf6c394086540b82a63197757e0583e7
arrizkyhp <arrizkyhasya@gmail.com>
2021-08-25 14:10:24 +0700

    Finished 3 Column Preview Card

--- 3-column-preview-card-component-main/src/App.js
+++ 3-column-preview-card-component-main/src/App.js
@@ -1,12 +1,14 @@
 import 'dist/css/style.css';
+import Home from 'parts/Home';
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Home />
+      </div>
+    </Router>
   );
 }
 
